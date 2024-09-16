4d066621b83c8ee42165c19aa9c51d91f61a4091
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-16 12:11:22 -0300

    test carga horaria menor que 1

--- test/CursoOnline.DominioTest/Cursos/CursoTest.cs
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -39,6 +39,24 @@ public class CursoTest
         Assert.Throws<ArgumentException>(() =>
             new Curso(nomeCursoInvalido, cursoEsperado.CargaHoraria, PublicoAlvo.Estudante, cursoEsperado.Valor));
     }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-100)]
+    public void NaoDeveTerCursoCargaHorariaMenorQue1(int cargaHorariaInvalida)
+    {
+        var cursoEsperado = new
+        {
+            Nome = "Informática",
+            CargaHoraria = 80,
+            PublicoAlvo = PublicoAlvo.Estudante,
+            Valor = (decimal) 2950
+        };
+        
+        Assert.Throws<ArgumentException>(() =>
+            new Curso(cursoEsperado.Nome, cargaHorariaInvalida, PublicoAlvo.Estudante, cursoEsperado.Valor));
+    }
 }
 
 public enum PublicoAlvo
@@ -59,6 +77,9 @@ public class Curso
     public Curso(string nome, int cargaHoraria, PublicoAlvo publicoAlvo, decimal valor)
     {
         if (string.IsNullOrEmpty(nome))
+            throw new ArgumentException();
+        
+        if (cargaHoraria < 1)
             throw new ArgumentException();
 
         Nome = nome;
