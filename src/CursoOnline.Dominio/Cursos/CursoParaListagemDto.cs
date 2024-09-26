9d5f15774faf95eef5323f996b96d86f5b6697d7
Matheus Gomes <math.s.gomes@gmail.com>
2024-09-25 22:23:48 -0300

    add method to list cursos on controller

--- src/CursoOnline.Dominio/Cursos/CursoParaListagemDto.cs
+++ src/CursoOnline.Dominio/Cursos/CursoParaListagemDto.cs
@@ -6,5 +6,5 @@ public class CursoParaListagemDto
     public string Nome { get; set; }
     public double CargaHoraria { get; set; }
     public string PublicoAlvo { get; set; }
-    public double Valor { get; set; }
+    public decimal Valor { get; set; }
 }
--- src/CursoOnline.Web/Controllers/CursoController.cs
+++ src/CursoOnline.Web/Controllers/CursoController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using CursoOnline.Dominio._Base;
 using CursoOnline.Dominio.Cursos;
 using Microsoft.AspNetCore.Mvc;
 using CursoOnline.Web.Models;
@@ -11,10 +12,35 @@ namespace CursoOnline.Web.Controllers;
 public class CursoController : Controller
 {
     private readonly ArmazenadorDeCurso _armazenadorDeCurso;
+    private readonly IRepositorio<Curso> _cursoRepositorio;
 
-    public CursoController(ArmazenadorDeCurso armazenadorDeCurso)
+    public CursoController(ArmazenadorDeCurso armazenadorDeCurso, IRepositorio<Curso> cursoRepositorio)
     {
         _armazenadorDeCurso = armazenadorDeCurso;
+        _cursoRepositorio = cursoRepositorio;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var cursos = _cursoRepositorio.Consultar();
+
+        if (cursos.Any())
+        {
+            // Nunca renderizar o domínio, sempre fazer a conversão para DTO
+            var dtos = cursos.Select(c => new CursoParaListagemDto
+            {
+                Id = c.Id,
+                Nome = c.Nome,
+                CargaHoraria = c.CargaHoraria,
+                PublicoAlvo = c.PublicoAlvo.ToString(),
+                Valor = c.Valor
+            });
+            
+            return Ok(dtos);
+        }
+
+        return NotFound("Nenhum curso encontrado");
     }
 
     [HttpPost]
