339d245957988b591a4da3e18e8e3be65989c8f7
Matheus Gomes <math.s.gomes@gmail.com>
2024-10-18 23:31:48 -0300

    feat: matricula builder

---
+++ test/CursoOnline.DominioTest/_Builders/MatriculaBuilder.cs
@@ -0,0 +1,49 @@
+using CursoOnline.Dominio.Alunos;
+using CursoOnline.Dominio.Cursos;
+using CursoOnline.DominioTest.Matriculas;
+
+namespace CursoOnline.DominioTest._Builders;
+
+public class MatriculaBuilder
+{
+    private Aluno _aluno { get; set; }
+    private Curso _curso { get; set; }
+    private decimal _valorPago { get; set; }
+
+    public static MatriculaBuilder Novo()
+    {
+        return new MatriculaBuilder
+        {
+            // Implemento valores default (poderiam ser fakes)
+            _aluno = AlunoBuilder.Novo().Build(),
+            _curso = CursoBuilder.Novo().Build(),
+            _valorPago = 1000m
+        };
+    }
+
+    public MatriculaBuilder ComAluno(Aluno aluno)
+    {
+        _aluno = aluno;
+        return this;
+    }
+
+    public MatriculaBuilder ComCurso(Curso curso)
+    {
+        _curso = curso;
+        return this;
+    }
+
+    public MatriculaBuilder ComValorPago(decimal valorPago)
+    {
+        _valorPago = valorPago;
+        return this;
+    }
+
+    public Matricula Build()
+    {
+        return new Matricula(
+            aluno: _aluno,
+            curso: _curso,
+            valorPago: _valorPago);
+    }
+}
