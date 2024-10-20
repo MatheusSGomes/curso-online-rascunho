bbe73f3cb5c29cf98b295ebb75f063aae9ce6b55
Matheus Gomes <math.s.gomes@gmail.com>
2024-10-20 19:39:44 -0300

    test: deve notificar quando aluno não for encontrado

--- src/CursoOnline.Dominio/_Base/Resource.cs
+++ src/CursoOnline.Dominio/_Base/Resource.cs
@@ -15,4 +15,5 @@ public static class Resource
     public static string ValorPagoMaiorQueValorCurso
         = "Valor pago na matrícula não pode ser maior do que o valor do curso";
     public static string PublicosAlvoDiferentes = "Público alvo do aluno e curso são diferentes";
+    public static string CursoNaoEncontrado = "Curso não encontrado";
 }
--- test/CursoOnline.DominioTest/Matriculas/CriacaoDaMatriculaTest.cs
+++ test/CursoOnline.DominioTest/Matriculas/CriacaoDaMatriculaTest.cs
@@ -13,14 +13,36 @@ public class CriacaoDaMatriculaTest
     private readonly Mock<ICursoRepositorio> _cursoRepositorio;
     private readonly Mock<IAlunoRepository> _alunoRepositorio;
     private readonly CriacaoDaMatricula _criacaoDaMatricula;
+    private readonly MatriculaDto _matriculaDto;
 
     public CriacaoDaMatriculaTest()
     {
         _cursoRepositorio = new Mock<ICursoRepositorio>();
         _alunoRepositorio = new Mock<IAlunoRepository>();
 
+        var aluno = AlunoBuilder.Novo().ComId(23).ComPublicoAlvo(PublicoAlvo.Universitario).Build();
+        var curso = CursoBuilder.Novo().ComId(1).ComPublicoAlvo(PublicoAlvo.Universitario).Build();
+
+        _alunoRepositorio.Setup(repo => repo.ObterPorId(aluno.Id)).Returns(aluno);
+        _cursoRepositorio.Setup(repo => repo.ObterPorId(curso.Id)).Returns(curso);
+
+        _matriculaDto = new MatriculaDto { AlunoId = aluno.Id, CursoId = curso.Id };
+
         // Instancia a Domain Service
         _criacaoDaMatricula = new CriacaoDaMatricula(_alunoRepositorio.Object, _cursoRepositorio.Object);
+    }
+
+    [Fact]
+    // Curso deve existir no banco de dados, caso não, retorna uma exceção com "curso não encontrado"
+    public void DeveNotificarQuandoCursoNaoForEncontrado()
+    {
+        Curso cursoInvalido = null!;
+
+        _cursoRepositorio.Setup(repo => repo.ObterPorId(It.IsAny<int>())).Returns(cursoInvalido);
+
+        Assert.Throws<ExcecaoDeDominio>(() =>
+                _criacaoDaMatricula.Criar(_matriculaDto))
+            .ComMensagem(Resource.CursoNaoEncontrado);
     }
 }
 
@@ -41,7 +63,7 @@ public class CriacaoDaMatricula
         var curso = _cursoRepositorio.ObterPorId(matriculaDto.CursoId);
 
         ValidadorDeRegra.Novo()
-            .Quando(aluno.PublicoAlvo != curso.PublicoAlvo, Resource.PublicosAlvoDiferentes)
+            .Quando(curso == null, Resource.CursoNaoEncontrado)
             .DispararExcecaoSeExistir();
     }
 }
